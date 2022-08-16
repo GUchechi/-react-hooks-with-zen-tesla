b7f3b1f320892a33201c60015d6df2d4d1b6ca59
GUchechi <godswilluchendu7@gmail.com>
2022-08-16 14:04:17 +0100

    added buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <div className="counter">
+        <h2>0</h2>
+        <div className="btnContainer">
+          <button>-1</button>
+          <button>-2</button>
+          <button>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
