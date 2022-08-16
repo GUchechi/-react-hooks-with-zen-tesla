09ffcf509dbb6f848275fdbb344908f1b8a9ac88
GUchechi <godswilluchendu7@gmail.com>
2022-08-16 14:41:02 +0100

    added onclick function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleDecrement = () => {
+    setCount(count - 1)
+  }
+
+  
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+
+
+  const handleReset = () => {
+    setCount(0)
+  }
+
+
   return (
     <div className="App">
       <div className="counter">
-        <h2>0</h2>
+        <h2>{count}</h2>
         <div className="btnContainer">
-          <button>-1</button>
-          <button>-2</button>
-          <button>Reset</button>
+          <button onClick={handleDecrement}>-1</button>
+          <button onClick={handleIncrement}>+1</button>
+          <button onClick={handleReset}>Reset</button>
         </div>
       </div>
     </div>
